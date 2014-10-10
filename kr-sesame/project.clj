7b50af90a267d04bf6a6bdfa809c77fdfa1901c6
Michael Gaare <mgaare@Michaels-MacBook-Pro.local>
2014-10-10 14:40:02 -0400

    Fix syntax error in project.clj of container projects

--- kr-sesame/project.clj
+++ kr-sesame/project.clj
@@ -1,4 +1,4 @@
 (defproject edu.ucdenver.ccp/kr-sesame "1.4.20-SNAPSHOT"
   :description "KR Sesame bindings."
   :parent [edu.ucdenver.ccp/kr "1.4.20-SNAPSHOT" :relative-path "../pom.xml"]
-  :dependencies [edu.ucdenver.ccp/kr-sesame-core "1.4.20-SNAPSHOT"])
+  :dependencies [[edu.ucdenver.ccp/kr-sesame-core "1.4.20-SNAPSHOT"]])
